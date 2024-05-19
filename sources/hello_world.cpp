31880adba18d99a31e1b3f2e9f52468f6d48f84d
SonoJy <sonojy2358@gmail.com>
2024-05-19 13:21:40 +0300

    lb02
    lb02 timp

---
+++ sources/hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+int
+main()
+{
+  std::string name;
+  std::cin >> name;
+  std::cout << "Hello World from" << name << "!";
+  // Fixed brackets
+  return 1;
+}
